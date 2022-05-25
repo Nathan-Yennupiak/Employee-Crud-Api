d5850f76d599e6bb69e6a903f5c210c5d7a5a178
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2022-05-24 18:44:23 -1200

    Added new Routes

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require ("express")
+const bodyParser = require("body-parser")
 const dotenv = require ("dotenv")
 const mongoose = require("mongoose")
 const connectDB = require('./config/db')
 
 //Initialize Express
 const app = express()
-
+app.use(bodyParser.json())
 //Routes
-app.get('/', (req, res) => {
-    console.log("Nathan is a prophet")
-    res.send("Nathan is a prophet")
-})
+// app.get('/', (req, res) => {
+//     console.log("Nathan is a prophet")
+//     res.send("Nathan is a prophet")
+// })
 //Load Config
 dotenv.config({path: './config/config.env'});
 connectDB()
+
+//Routes
+app.use('/', require('./routes/index'))
 
 
 app.listen(3000);
---
+++ routes/index.js
@@ -0,0 +1,45 @@
+const express = require ("express")
+const router = express.Router()
+
+const { Employee } = require('../models/employee')
+
+//Get All Employees
+router.get('/api/employees', (req, res) => {
+    Employee.find({}, (err, data) => {
+        if(!err) {
+            res.send(data)
+        }else {
+            console.log(err)
+        }
+    })
+}) 
+
+//Save Employee
+router.post('/api/employee/add', (req, res) => {
+    const emp = new Employee({
+        name: req.body.name,
+        email: req.body.email,
+        salary: req.body.salary
+    })
+    emp.save((err, data) => {
+        if(!err) {
+            res.status(200).json({ code: 200, message: "Employee Added Successfully", addEmployee:data})
+        }else {
+            console.log(err)
+        }
+        
+    })
+})
+
+//Get A Single Employee
+router.get('/api/employee/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, data) => {
+        if(!err) {
+            res.send(data)
+        } else {
+            console.log(err)
+        }
+    })
+})
+module.exports = router;
+
