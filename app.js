d3e701e19c2c3b39918188867ce069120ca5ace0
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2022-05-29 10:41:16 -1200

    config fies hidden

--- app.js
+++ app.js
@@ -7,16 +7,11 @@ const connectDB = require('./config/db')
 //Initialize Express
 const app = express()
 app.use(bodyParser.json())
-//Routes
-// app.get('/', (req, res) => {
-//     console.log("Nathan is a prophet")
-//     res.send("Nathan is a prophet")
-// })
-//Load Config
+
 dotenv.config({path: './config/config.env'});
 connectDB()
 
-//Routes
+//Routes.
 app.use('/', require('./routes/index'))
 
 
